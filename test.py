074581fb4b86ec2700fb210a9122d249dff6222c
Kripa George <kripaageorge@gmail.com>
2023-03-30 10:56:08 -0500

    test.py

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 from ctypes import *
-so_file = "home/pi/list.so"
+import os
+
+cwd = os.getcwd()
+rel_path = os.path.join(cwd, "list.so")
+so_file = rel_path
 my_functions = CDLL(so_file) 
 
-my_functions.list()
+def get_namespaces():
+    namespaces = my_functions.list()
+    return namespaces
