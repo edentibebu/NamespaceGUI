654c9dac2c53cedfe2589ba4c69d594221990757
Kripa George <kripaageorge@gmail.com>
2023-03-31 16:05:58 -0500

    show namespaces as buttons

--- home.py
+++ home.py
@@ -10,6 +10,7 @@ root.title("Namespace GUI: Home")
 #namespace_heading = Label(root, text="Namespaces")
 #namespace_heading.pack()
 
+############################ INTERFACING WITH PI #################################
 def get_namespaces():
     output = []
     output = subprocess.check_output("ip netns", shell=True)
@@ -17,6 +18,15 @@ def get_namespaces():
     namespaces = output.decode()
     return namespaces
 
+def get_cap(ns):
+    output = []
+
+    output = subprocess.check_output("ip netns exec" + str(ns) + "capsh --print", shell=True)
+    caps = output.decode()
+    print(caps)
+    return caps
+
+########################### WINDOWS #####################################
 #new window to add namespace on add-ns button
 def add_ns_window():
         add_ns_window = Toplevel(root)
@@ -30,14 +40,16 @@ def ns_view(ns): #TODO: pass in namespace name
     ns_header = Label(ns_view, text=ns) #TODO: pass in namespace name somehow and print it here
 
     #creating frames
-    cap_frame = LabelFrame(ns_view, text="Namespaces", padx=5, pady=5)
-    #namespace_frame.pack()
+    cap_frame = LabelFrame(ns_view, text=ns, padx=5, pady=5)
     cap_frame.grid(row = 0, column = 0, padx=10, pady=10)
 
     process_frame = LabelFrame(ns_view, text="Processes", padx=5, pady=5)
     process_frame.grid(row=0, column=1, padx=50, pady = 10)
 
     # TODO: get capabilities for this namespace with C code
+
+    capabilities = get_cap()
+    print(capabilities)
     cap = Label(cap_frame, text="capability 1")
     cap.grid(row=0, column=0, padx=5, pady=5)
     cap_en = IntVar() #set up a list of IntVar(), corresponding with each cap
@@ -45,10 +57,10 @@ def ns_view(ns): #TODO: pass in namespace name
     toggle.grid(row=0, column=1, padx=20, pady= 5)
 
     #TODO: iterate through list of processes
-    proc = Label(process_frame, text="Processes in [Namespace Name]" ) #TODO: get namespace name and insert here
+    proc = Label(process_frame, text="Processes in" ) #TODO: get namespace name and insert here
     proc.grid(row=0, column=0)
 
-
+##################################### FRAMES #########################################
 #creating frames
 namespace_frame = LabelFrame(root, text="Namespaces", padx=5, pady=5)
 #namespace_frame.pack()
@@ -58,16 +70,20 @@ process_frame = LabelFrame(root, text="Processes", padx=5, pady=5)
 #process_frame.pack()
 process_frame.grid(row=0, column=1, padx=50, pady = 10)
 
-#TODO: List namespaces
+################################## List namespaces ##################################
 # get namespaces as list from C code
-namespaces = get_namespaces()
-print("Got namespaces! they are : ")
-ns_list = namespaces.split('\n')[:-1]
+# namespaces = get_namespaces()
+# print("Got namespaces! they are : ")
+# ns_list = namespaces.split('\n')[:-1]
+
+ns_list = ['ns1', 'ns2']
 
 for i, ns in enumerate(ns_list):
-    ns_btn = Button(namespace_frame, text=ns, command=ns_view(ns)) #TODO: clicking on button brings up NS-view.py for editing
+    ns_btn = Button(namespace_frame, text=ns, command=lambda ns=ns: ns_view(ns)) #TODO: clicking on button brings up NS-view.py for editing
     ns_btn.grid(row = i+1, column = 0) # TODO: row will change for each namespace, column will not. add padding around text
 
+
+############################### Home 
 add_ns_btn = Button(namespace_frame, text="Add Namespace", command = add_ns_window)
 add_ns_btn.grid(row=0, column=1)
 
