9e58887816105cfb911de1d39099a81bf3ba9b1f
Kripa George <kripaageorge@gmail.com>
2023-03-31 16:30:40 -0500

    show namespaces as buttons

--- home.py
+++ home.py
@@ -36,7 +36,7 @@ def add_ns_window():
 
 #new window to view a namespace (on click of namespace name)
 def ns_view(ns): #TODO: pass in namespace name
-    # ns_view = Toplevel(root)
+    ns_view = Toplevel(root)
     #ns_view = tk.Canvas(root)
     ns_view.title("Namespace GUI: Namespace View")
     #scrollbar = ttk.Scrollbar(root, orient="vertical", command=ns_view.yview)
