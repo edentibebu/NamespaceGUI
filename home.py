47836c8c62de6acafb372cd3544d5ea6f0012f00
Kripa George <kripaageorge@gmail.com>
2023-04-01 12:48:47 -0500

    show processes for a namespace

--- home.py
+++ home.py
@@ -43,8 +43,8 @@ def get_cap(ns):
 
 def get_procs(ns):
     output = subprocess.check_output("ps u $(ip netns pids " + str(ns) + ")", shell=True)
-    print(procs)
-    print(type(procs))
+    print(output)
+    print(type(output))
     procs = output.decode()
     return procs
 
