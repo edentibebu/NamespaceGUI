1176c3c37d8e42da83e00cfc2c2942f8f76f4023
Kripa George <kripaageorge@gmail.com>
2023-04-01 13:13:40 -0500

    put processes into tabular format

--- home.py
+++ home.py
@@ -95,7 +95,7 @@ def ns_view(ns): #passing in ns name
 
     #TODO: iterate through list of processes
     procs = get_procs(ns)
-    header = procs[0].split('  ')
+    header = procs[0].split(' ')
     body = procs[1:]
     for i, line in enumerate(body):
         line = line.split('   ')
