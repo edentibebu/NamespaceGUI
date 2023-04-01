0bd05632c527a50f2d32ab97d99a7761ab3b8f20
edentibebu <etibebu@wustl.edu>
2023-04-01 13:32:54 -0500

    top 5 cpu/mem added

--- test.py
+++ test.py
@@ -38,18 +38,39 @@ def delete_ns():
 
 def remove_cap():
     output = []
-    output = subprocess.check_output("sudo setcap -r testing", shell=True)
-    remove = output.decode()
-    return remove
+    network = subprocess.check_output("sudo ip netns exec newns setcap -r /home/eden/NamespaceGUI cap_perfmon+ep", shell=True)
+    #  "sudo lsns -t net | grep testing | awk '{print $1}'", shell=True)
+    ID = network
+    # print(ID)
+    # output = subprocess.check_output("sudo setcap -r CAP_SYS_TIME+ep /proc/"+ str(ID) +"/ns/net", shell=True)
+    # remove = output.decode()
+    return ID
 
 def get_procs():
     output = subprocess.check_output("ps u $(ip netns pids testing)", shell=True)
     procs = output.decode()
+    print(output)
     return procs
 
+def top_5_cpu():
+    output = subprocess.check_output("ps -eo pid,ppid,%cpu,%mem,cmd --sort=-%cpu | head -n 6", shell=True)
+    cpu = output.decode()
+    print(cpu)
+    return cpu
+
+def top_5_mem():
+    output = subprocess.check_output("ps -eo pid,ppid,%cpu,%mem --sort=-%mem | head -n 6", shell=True)
+    mem = output.decode()
+    print(mem)
+    return mem
+
+# top_5_mem()
+
+
+# top_5_cpu()
 # remove_cap()
 # get_namespaces()
 # get_cap()
 # add_ns()
 # delete_ns()
-# get_procs()
+get_procs()
