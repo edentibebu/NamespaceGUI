f0ea711012d66e060f3a3b7667392a371d4b0d10
edentibebu <etibebu@wustl.edu>
2023-04-20 17:14:15 -0500

    port forwarding done

--- test.py
+++ test.py
@@ -67,26 +67,67 @@ def get_procs():
     print(output)
     return procs
 
-def top_5_cpu():
-    output = subprocess.check_output("ps -eo pid,ppid,%cpu,%mem,cmd --sort=-%cpu | head -n 6", shell=True)
+def top_5_cpu(ns):
+    output = subprocess.check_output("ip netns exec "+str(ns)+" ps -eo pid,ppid,%cpu,%mem,cmd --sort=-%cpu | head -n 6", shell=True)
     cpu = output.decode()
     print(cpu)
     return cpu
 
-def top_5_mem():
-    output = subprocess.check_output("ps -eo pid,ppid,%cpu,%mem --sort=-%mem | head -n 6", shell=True)
+def top_5_mem(ns):
+    output = subprocess.check_output("ip netns exec "+str(ns)+" ps -eo pid,ppid,%cpu,%mem --sort=-%mem | head -n 6", shell=True)
     mem = output.decode()
     print(mem)
     return mem
 
+def disp_routing():
+    output = subprocess.check_output("ip netns exec ns1 route", shell=True)
+    routes = output.decode()
+    print(routes)
+    return routes
+
+def list_sockets():
+    output = subrocess.check_output("ip netns exec ns1 ss -n -a", shell=True)
+    sockets = output.decode()
+    print(sockets)
+    return sockets
+
+def bridge(bridge):
+    output = subprocess.check_output("sudo ip link add name "+str(bridge)+" type bridge", shell=True)
+    return output
+
+def list_veth_pairs():
+    output = subprocess.check_output("ip link", shell=True)
+    veth_list = output.decode()
+    return veth_list
+
+def port_forward(ns, device1, device2, ip1, ip2, port1, port2):
+    output = subprocess.check_output("sudo sysctl -w net.ipv4_forward=1; sudo iptables -t nat -A PREROUTING -p tcp --dport "+str(port1)+" -j DNAT --to-destination "+str(ip1)+ ":"+str(port2)+"", shell=True)
+    print("port forwarded")
+    return output
+
+
+#def tcp():
+#   port = "7096"
+#    ns1 = "1"
+#    ns2 = "2"
+#    ip = "10.1.1.1"
+#    output = subprocess.check_output("ip netns exec "+str(ns1)+" nc -l "+str(ip)+" "+str(port)+" -v", shell=True)
+    # open another terminal to connect second namespace to first
+    # then run command to allow communication between two namespaces
+    # show messages sent
+    # possibly show tcpdump to grab all packets transmitted between two namespaces
+
+# forward traffic using ports
+# window to show port forwarding worked (html)
+
 # top_5_mem()
 
-
-# top_5_cpu()
+port_forward()
+#top_5_cpu()
 # remove_cap()
 # get_namespaces()
 # get_cap()
 # add_ns()
 # delete_ns()
 # get_procs()
-set_ips()
+# set_ips()
