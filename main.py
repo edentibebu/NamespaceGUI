8bd4acbc22ea0f88d8fdb177de10d18a08d8482b
Kripa George <kripaageorge@gmail.com>
2023-04-20 16:38:59 -0500

    moving onto namespace view details

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+import home, utils
+
+root = Tk()
+root.title("Namespace GUI: Home")
+
+home.Home(root)
+
+root.mainloop()
--- ns_view.py
+++ ns_view.py
@@ -1,5 +1,19 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
-import utils, home
+import utils
+
 #new window to view a namespace (on click of namespace name)
+def net_ns_view(ns):
+
+    #TODO: make this page scrollable!!
+    ns_view = Toplevel(root)
+    #ns_view = tk.Canvas(root)
+    ns_view.title("Namespace GUI: Namespace View")
+    #scrollbar = ttk.Scrollbar(root, orient="vertical", command=ns_view.yview)
+
+    if(utils.checkuid[0] != "0"):
+        Label(ns_view, text = "Sorry, you cannot access this window because you do not have root privileges").pack()
+        return
+
+    net_ns_header = Label(ns_view, text=ns)
