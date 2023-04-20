874dfc1b7e6083ca17816dc4621e48e85450f81f
Kripa George <kripaageorge@gmail.com>
2023-04-20 15:38:26 -0500

    reverting back

--- add_net_ns.py
+++ add_net_ns.py
@@ -3,6 +3,7 @@ from tkinter import *
 import tkinter as tk
 from tkinter import ttk
 import utils
+import ns_view
 
 def add_ns(ns_name):
     command_str = "ip netns add " + str(ns_name)
@@ -19,9 +20,13 @@ def add_veth(netns, device1, device2):
 def set_ips(netns, device1, device2, ip1, ip2):
     subprocess.check_output("sudo ip netns exec "+str(netns)+" ifconfig " +str(device2)+" "+str(ip2)+" up; sudo ifconfig "+str(device1)+" " +str(ip1)+" up; ping "+str(ip2)+"; sudo ip netns exec "+str(netns)+" ping "+str(ip2), shell=True)
 
-def update_ns_list(net_namespace_frame):
-    utils.clear_namespaces(net_namespace_frame)
-    utils.list_namespaces(net_namespace_frame)
+def update_ns_list(net_namespace_frame, ns_name):
+    net_ns = utils.get_net_namespaces()
+    num_ns = len(net_ns.split('\n')[:-1])
+    ns_btn = Button(net_namespace_frame, text=ns_name, command=lambda ns=ns_name: ns_view.net_ns_view(ns)) #TODO: clicking on button brings up NS-view.py for editing
+    ns_btn.grid(row = num_ns+1, column = 0) # TODO: row will change for each namespace, column will not. add padding around text
+    #utils.clear_namespaces(net_namespace_frame)
+    #utils.list_namespaces(net_namespace_frame)
     #net_namespace_frame.grid(row = 0, column = 0, padx=10, pady=10)
     net_namespace_frame.update()
 
