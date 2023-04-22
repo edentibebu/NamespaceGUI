cb67358414afc38d13c055fa1bcd26a1f7e38076
Kripa George <kripaageorge@gmail.com>
2023-04-22 14:32:48 -0500

    adding dropdown

--- utils.py
+++ utils.py
@@ -135,7 +135,7 @@ def get_ns(ns_name):
     # ns = ns.split('(id')[0]
     print(ns_name)
     ns_list = list(filter(lambda s: s != "", ns_list)).remove(ns_name)
-    
+    print(ns_list)
     return ns_list
     #get subnet for each ns:
     for ns in ns_list:
