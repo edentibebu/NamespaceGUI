729b69fccef000c445b51739bb6882338b5f910a
Kripa George <kripaageorge@gmail.com>
2023-04-22 11:12:16 -0500

    redoing connections

--- add_device.py
+++ add_device.py
@@ -14,35 +14,35 @@ class AddDevice:
         self.add_device_window()
 
     def add_device_window(self):
-        add_net_ns_window = Toplevel(self.root)
-        add_net_ns_window.title("Add Device to Namespace")
+        add_device_window = Toplevel(self.root)
+        add_device_window.title("Add Device to Namespace")
         if(utils.checkuid[0] == "0"):
             print(self.ns)
             Label(self.ns_view, text = self.ns).grid(row=0, column=1)
 
             #TODO: add functionality to make placeholder text grey that goes away after clicking in cell
             
-            Label(add_net_ns_window, text ="VEth Pairs:").grid(row=2, column=0)
-            Label(add_net_ns_window, text ="Device 1:").grid(row=1, column=1)
-            Label(add_net_ns_window, text ="Device 2:").grid(row=1, column=2)
-            device1 = Entry(add_net_ns_window)
+            Label(add_device_window, text ="VEth Pairs:").grid(row=2, column=0)
+            Label(add_device_window, text ="Device 1:").grid(row=1, column=1)
+            Label(add_device_window, text ="Device 2:").grid(row=1, column=2)
+            device1 = Entry(add_device_window)
             device1.grid(row=2, column=1)
-            device2 = Entry(add_net_ns_window)
+            device2 = Entry(self.ns_view)
             device2.grid(row=2, column=2)
 
-            Label(add_net_ns_window, text ="IP Addresses:").grid(row=4, column=0)
-            Label(add_net_ns_window, text ="Address 1:").grid(row=3, column=1)
-            Label(add_net_ns_window, text ="Address 2:").grid(row=3, column=2)
+            Label(add_device_window, text ="IP Addresses:").grid(row=4, column=0)
+            Label(add_device_window, text ="Address 1:").grid(row=3, column=1)
+            Label(add_device_window, text ="Address 2:").grid(row=3, column=2)
 
-            ip1 = Entry(add_net_ns_window)
+            ip1 = Entry(add_device_window)
             ip1.grid(row=4, column=1)
-            ip2 = Entry(add_net_ns_window)
+            ip2 = Entry(add_device_window)
             ip2.grid(row=4, column=2)
-            add_ns_btn = Button(add_net_ns_window, text='Submit', command = lambda: self.add_device(self.ns, device1, device2, ip1, ip2))
+            add_ns_btn = Button(add_device_window, text='Submit', command = lambda: self.add_device(add_device_window, device1, device2, ip1, ip2))
             add_ns_btn.grid(row=5, column=4)
             
         else:
-            Label(add_net_ns_window, text = "Sorry, you cannot access this window because you do not have root privileges").pack()
+            Label(add_device_window, text = "Sorry, you cannot access this window because you do not have root privileges").pack()
     def add_device(self, ns_name, device1, device2, ip1, ip2):
         print(ns_name)
         # device1 = device1.get()
