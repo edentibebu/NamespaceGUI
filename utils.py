6a639520774e461a4d1e7e47308eedcae9723fb0
Kripa George <kripaageorge@gmail.com>
2023-04-25 09:33:44 -0500

    removing devices

--- utils.py
+++ utils.py
@@ -92,7 +92,8 @@ def show_added_ns(ns_name, net_namespace_frame, root):
 
 def show_remove_ns(net_namespace_frame, root):
     net_ns = get_net_namespaces()
-    net_namespace_frame.destroy()
+    for widget in net_namespace_frame.winfo_children():
+        widget.destroy()
     net_namespace_frame = LabelFrame(root, text="Network Namespaces", padx=5, pady=5)
     net_namespace_frame.grid(row = 0, column = 0, padx=10, pady=10)
     # List namespaces 
