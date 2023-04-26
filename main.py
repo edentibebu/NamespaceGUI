767499794707ddc8ccededc57c15cb5fbb7691b8
RicoC-WU <ricojcole@gmail.com>
2023-04-25 21:15:48 -0500

    GUI detection

--- main.py
+++ main.py
@@ -7,15 +7,20 @@ import threading
 def inotify():
     inotify_process = subprocess.Popen(["sudo", "./inotify_gui", "/var/run/netns", "output.txt"])
     filename = 'output.txt'
+    gui_log = open('gui_log.txt', 'r')
     time.sleep(2)
     last_modified = os.path.getmtime(filename)
     while(True):
         current_modified = os.path.getmtime(filename)
         if last_modified != current_modified:
-            with open(filename, 'r') as file:
-                lines = file.readlines()
+            with open(filename, 'r') as f:
+                lines = f.readlines()
                 if lines:
                     last_line = lines[-1]
+                    gui_lines = gui_log.readlines()
+                    if gui_lines:
+                        if last_line == gui_lines[-1]:
+                            print("GUI!!!")
                     utils.show_alert(last_line)
                     print("last line", last_line)
                 last_modified = current_modified
