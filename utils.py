0e75371bdc1f44989c7deb46cceeff2db4e20c81
Kripa George <kripaageorge@gmail.com>
2023-04-25 22:14:58 -0500

    issues

--- utils.py
+++ utils.py
@@ -232,7 +232,7 @@ def get_ns(ns_name):
     return ns_list
 
 def enable_ns_to_host_ip_forwarding(port1, port2):
-    print("enable_ns_to_ns_ip_forwarding")
+    print("enable_ns_to_host_ip_forwarding")
     subprocess.run("sysctl -w net.ipv4.ip_forward=1", shell=True)
     subprocess.check_output("iptables -t nat -A OUTPUT -p tcp --dport "+str(port1)+" -j DNAT --to-destination 127.0.0.1:"+str(port2), shell=True)
 
@@ -240,7 +240,7 @@ def enable_ns_to_ns_ip_forwarding(subnet, device1, device2, port1, port2):
     print("enable_ns_to_ns_ip_forwarding")
     ip2 = subnet + device2
     subprocess.run("sysctl -w net.ipv4.ip_forward=1", shell=True)
-    result = subprocess.run("iptables -t nat -A PREROUTING -i "+str(device1)+" -p tcp --dport "+str(port1)+" -j DNAT --to-destination "+str(ip2)+":"+str(port2), text=True, check_output=True, shell=True)
+    result = subprocess.run("iptables -t nat -A PREROUTING -i "+str(device1)+" -p tcp --dport "+str(port1)+" -j DNAT --to-destination "+str(ip2)+":"+str(port2), text=True, capture_output=True, shell=True)
     if result.returncode != 0:
         show_alert(result.stderr)
         return
